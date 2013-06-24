d6a88d7162c3b4da803fb0f8ec2c0b02813f7e0b
Luke Stuff <info@indiegameadventures.com>
2013-06-24 12:46:48 -0700

    Added Android LogCat support

--- FutureEngine/src/core/debug/log.cpp
+++ FutureEngine/src/core/debug/log.cpp
@@ -2,6 +2,9 @@
 #include <stdarg.h>
 #include <wchar.h>
 
+#if FUTURE_PLATFORM_ANDROID
+#	include <android/log.h>
+#endif
 
 // default assert to be used unless another function is specified
 static void FutureLogFunctionDefault(u8 severness, string file, u32 line, string message, ...)
@@ -40,7 +43,41 @@ static void FutureLogFunctionDefault(u8 severness, string file, u32 line, string
 
 	file = wcsrchr(file, '\\' );
 	file = (string)((u32)file + sizeof(wchar_t));
+
+#if FUTURE_PLATFORM_ANDROID
+	char androidBuffer[2048];
+	char androidFile[256];
+	count = sprintf(androidBuffer, FUTURE_ARRAY_LENGTH(androidBuffer), "%ls: [%ls:%d] %ls", se, file, line, buffer);
+	if(count > FUTURE_ARRAY_LENGTH(androidBuffer) || count < 0)
+	{
+		FUTURE_DEBUG_HALT();
+	}
+	count = sprintf(androidFile, FUTURE_ARRAY_LENGTH(androidFile), "%ls", file);
+	if(count > FUTURE_ARRAY_LENGTH(androidFile) || count < 0)
+	{
+		FUTURE_DEBUG_HALT();
+	}
+	switch(severness)
+	{
+	case FutureMessageSeverness::MESSAGE_VERBOSE:
+		__android_log_print(ANDROID_LOG_VERBOSE, androidFile, androidBuffer);
+		break;
+	case FutureMessageSeverness::MESSAGE_INFO:
+		__android_log_print(ANDROID_LOG_INFO, androidFile, androidBuffer);
+		break;
+	case FutureMessageSeverness::MESSAGE_DEBUG:
+		__android_log_print(ANDROID_LOG_DEBUG, androidFile, androidBuffer);
+		break;
+	case FutureMessageSeverness::MESSAGE_WARNING:
+		__android_log_print(ANDROID_LOG_WARNING, androidFile, androidBuffer);
+		break;
+	case FutureMessageSeverness::MESSAGE_ERROR:
+		__android_log_print(ANDROID_LOG_ERROR, androidFile, androidBuffer);
+		break;
+	}
+#else
 	wprintf_s(L"%ls: [%ls:%d] %ls\n", se, file, line, buffer);
+#endif
 }
 
 
